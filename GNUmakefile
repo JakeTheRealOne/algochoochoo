6bd1d1dafd611efbf32568aee7a663f76475e1c8
Bilal Vandenberge <jakelevrai@outlook.be>
2025-04-16 19:24:15 +0200

    [ADD] Main files

---
+++ GNUmakefile
@@ -0,0 +1,16 @@
+SOURCE_DIR := source
+BUILD_DIR := build
+
+SOURCES := $(shell find $(SOURCE_DIR) -name "*.java")
+
+all: $(BUILD_DIR)/.classes
+
+$(BUILD_DIR)/.classes: $(SOURCES)
+	@mkdir -p $(BUILD_DIR)
+	javac -d $(BUILD_DIR) $(SOURCES)
+#	@touch $(BUILD_DIR)/.class
+
+clean:
+	rm -rf $(BUILD_DIR)
+
+.PHONY: all clean
---
+++ source/Main.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.LookupCSV;
+
+public static final String TEST_FILE = "assets/GTFS/DELIJN/routes.csv";
+
+/**
+ * Main class of the project
+ * Print the shortest path based on assets/GTFS data and certain criteria
+ * 
+ * @author Bilal Vandenberge
+ */
+public class Main {
+  /**
+   * Main entry point to the project
+   *
+   * @param args Command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println(TEST_FILE);
+  }
+}
+
+// TODO: Decide if we give assets/GTFS or not
